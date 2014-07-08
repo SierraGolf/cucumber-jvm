47fac3b9b704909b93cf5c3b0a568af98f9c65a6
Sebastian Gröbler <s.groebler@googlemail.com>
2014-07-08 10:49:24 +0200

    added espresso support

--- android/src/main/java/cucumber/api/android/CucumberInstrumentation.java
+++ android/src/main/java/cucumber/api/android/CucumberInstrumentation.java
@@ -1,51 +1,23 @@
 package cucumber.api.android;
 
-import android.app.Activity;
 import android.app.Instrumentation;
 import android.os.Bundle;
-import android.os.Looper;
-import cucumber.runtime.android.Arguments;
-import cucumber.runtime.android.CoverageDumper;
-import cucumber.runtime.android.CucumberExecutor;
-import cucumber.runtime.android.Waiter;
 
 
 public class CucumberInstrumentation extends Instrumentation {
-    public static final String REPORT_VALUE_ID = "CucumberInstrumentation";
-    public static final String REPORT_KEY_NUM_TOTAL = "numtests";
-    public static final String TAG = "cucumber-android";
 
-
-    private Waiter waiter;
-    private CoverageDumper coverageDumper;
-    private CucumberExecutor cucumberExecutor;
-    private Arguments arguments;
+    private final CucumberInstrumentationCore instrumentationCore = new CucumberInstrumentationCore(this);
 
     @Override
-    public void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
-
-        arguments = new Arguments(bundle);
-        cucumberExecutor = new CucumberExecutor(arguments, this);
-        coverageDumper = new CoverageDumper(arguments);
-        waiter = new Waiter(arguments);
+    public void onCreate(final Bundle arguments) {
+        super.onCreate(arguments);
+        instrumentationCore.create(arguments);
         start();
     }
 
     @Override
     public void onStart() {
-        Looper.prepare();
-
-        final Bundle results = new Bundle();
-        if (arguments.isCountEnabled()) {
-            results.putString(Instrumentation.REPORT_KEY_IDENTIFIER, REPORT_VALUE_ID);
-            results.putInt(REPORT_KEY_NUM_TOTAL, cucumberExecutor.getNumberOfTests());
-            finish(Activity.RESULT_OK, results);
-        } else {
-            waiter.requestWaitForDebugger();
-            cucumberExecutor.execute();
-            coverageDumper.requestDump(results);
-            finish(Activity.RESULT_OK, results);
-        }
+        instrumentationCore.start();
     }
+
 }
---
+++ android/src/main/java/cucumber/api/android/CucumberInstrumentationCore.java
@@ -0,0 +1,50 @@
+package cucumber.api.android;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.os.Bundle;
+import android.os.Looper;
+import cucumber.runtime.android.Arguments;
+import cucumber.runtime.android.CoverageDumper;
+import cucumber.runtime.android.CucumberExecutor;
+import cucumber.runtime.android.Waiter;
+
+
+public class CucumberInstrumentationCore {
+    public static final String REPORT_VALUE_ID = "CucumberInstrumentation";
+    public static final String REPORT_KEY_NUM_TOTAL = "numtests";
+
+
+    private final Instrumentation instrumentation;
+    private Waiter waiter;
+    private CoverageDumper coverageDumper;
+    private CucumberExecutor cucumberExecutor;
+    private Arguments arguments;
+
+    public CucumberInstrumentationCore(final Instrumentation instrumentation) {
+        this.instrumentation = instrumentation;
+    }
+
+    public void create(final Bundle bundle) {
+        arguments = new Arguments(bundle);
+        cucumberExecutor = new CucumberExecutor(arguments, instrumentation);
+        coverageDumper = new CoverageDumper(arguments);
+        waiter = new Waiter(arguments);
+    }
+
+    public void start() {
+        Looper.prepare();
+
+        final Bundle results = new Bundle();
+        if (arguments.isCountEnabled()) {
+            results.putString(Instrumentation.REPORT_KEY_IDENTIFIER, REPORT_VALUE_ID);
+            results.putInt(REPORT_KEY_NUM_TOTAL, cucumberExecutor.getNumberOfTests());
+        } else {
+            waiter.requestWaitForDebugger();
+            cucumberExecutor.execute();
+            coverageDumper.requestDump(results);
+        }
+
+        instrumentation.finish(Activity.RESULT_OK, results);
+    }
+}
